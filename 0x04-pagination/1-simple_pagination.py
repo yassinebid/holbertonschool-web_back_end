1a1c616a0cb02363412e326c6707e49689984af2
yassinebid <85518784+yassinebid@users.noreply.github.com>
2022-06-14 23:36:01 +0100

    test

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        pass
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """
+        return a tuple of size two containing a start index and an end index
+        """
+        end: int = page * page_size
+        start: int = 0
+        for _ in range(page - 1):
+            start += page_size
+        return (start, end)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        get page Simple pagination
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        dataset = self.dataset()
+        start, end = self.index_range(page, page_size)
+        if end > len(dataset):
+            return []
+        return [list(dataset[row]) for row in range(start, end)]
